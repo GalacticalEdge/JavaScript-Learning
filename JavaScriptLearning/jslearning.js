fc892b73e2a3771c291bb95f8740d02a95c0431f
Galactical Edge <31458662+GalacticalEdge@users.noreply.github.com>
2022-07-02 20:33:22 -0700

    Add files via upload

--- JavaScriptLearning/jslearning.js
+++ JavaScriptLearning/jslearning.js
@@ -1,3 +1,9 @@
 function myFunction() {
     document.getElementById("demo").innerHTML = "My First JavaScript";
 }
+
+function myFunction() {
+    document.getElementById("math").innerHTML = 5 + 6 
+}
+
+// Left off on JS Statements
